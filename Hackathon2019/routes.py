f0a9bb1ee5f4254ad6fc26533f7d884794ba28e1
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-03-02 20:18:26 -0500

    Update routes.py

--- Hackathon2019/routes.py
+++ Hackathon2019/routes.py
@@ -1,4 +1,8 @@
 # I don't really know what I'm doing but here goes nothing
+# To run locally, enter the Hackathon2019/Hackathon2019/ directory and run:
+# >set FLASK_APP=routes.py
+# >flask run
+# Then paste the URL to a web browser
 
 from flask import Flask
 from flask import render_template
