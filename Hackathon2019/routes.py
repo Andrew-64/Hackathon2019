0c60f0e7705bc38b416e75268808ca18600069e0
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-03-02 20:07:26 -0500

    Getting Flask

---
+++ Hackathon2019/routes.py
@@ -0,0 +1,4 @@
+# I don't really know what I'm doing but here goes nothing
+
+from flask import Flask
+
--- Hackathon2019/venv/Scripts/Activate.ps1
+++ Hackathon2019/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Andrew\Documents\Other\Hackathon2019\venv"
+$env:VIRTUAL_ENV="C:\Users\Andrew\Documents\GitHub\Hackathon2019\Hackathon2019\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
