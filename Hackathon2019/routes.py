96ada8cc3541fe6c27131d31006ad9c5123a2984
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-03-02 20:13:55 -0500

    Basic Test

--- Hackathon2019/routes.py
+++ Hackathon2019/routes.py
@@ -1,4 +1,11 @@
 # I don't really know what I'm doing but here goes nothing
 
 from flask import Flask
+from flask import render_template
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
